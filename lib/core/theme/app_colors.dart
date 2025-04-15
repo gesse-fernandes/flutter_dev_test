fad76111bbaf25c8758cb1ff4915a087dca28dfd
gesse-fernandes <gesse-fm@hotmail.com>
2025-04-15 12:27:02 -0300

    feat: criação cores e estilos para padronizar com o figma

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const backgroundDark = Color(0xFF212229);
+  static const backgroundLight = Color(0xFFF8F8FA);
+  static const backgroundMedium = Color(0xFFE7E7EF);
+  static const backgroundSurface = Color(0xFFF6F6F8);
+  static const error = Colors.red;
+  static const foreground = Color(0xFF494A57);
+  static const secondaryText = Color(0xFF9496AA);
+  static const brandBrown = Color(0xFF7A5D3E);
+  static const white = Colors.white;
+}
---
+++ lib/core/theme/text_styles.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'app_colors.dart';
+
+class AppTextStyles {
+  static const heading20 = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w500,
+    fontFamily: 'PlusJakartaSans',
+    color: AppColors.backgroundDark,
+  );
+  static const heading16 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+    fontFamily: 'PlusJakartaSans',
+    color: AppColors.backgroundDark,
+  );
+
+  static const heading14 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+    fontFamily: 'PlusJakartaSans',
+    color: AppColors.backgroundDark,
+  );
+
+  static const heading12 = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w500,
+    fontFamily: 'PlusJakartaSans',
+    color: AppColors.backgroundDark,
+  );
+}
